60b7768c0258fcd82c46a27ab5a51935a390118d
First-name Family-name <221205082@ccmailg.meijo-u.ac.jp>
2023-09-05 15:42:04 +0900

    3かいめのコミット 2日目のスライドの内容が終わったところ

--- app/src/main/java/jp/ac/meijou/android/aclassapp/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/aclassapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,8 +14,9 @@ import jp.ac.meijou.android.aclassapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
 
+    private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
     private int imageRes = R.drawable.ic_android;
 
     @Override
@@ -22,12 +24,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore = prefDataStore.getInstance(this);
 
-        binding.text.setText(R.string.my_name);
+
+
+        binding.saveButton.setOnClickListener(view ->{
+            var text = binding.editText.getText().toString();
+            prefDataStore.setString("name",text);
+            Log.d("Saved","save:"+binding.editText);
+        });
+
+
+
+      //  binding.text.setText(R.string.my_name);
 
         binding.buttonText.setOnClickListener(View ->{
             var text = binding.editText.getText().toString();
-            binding.text.setText(text);
+            binding.textField.setText(text);
         });
 
         binding.editText.addTextChangedListener(new TextWatcher() {
@@ -43,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                binding.text.setText(s.toString());
+                binding.textField.setText(s.toString());
             }
         });
 
@@ -55,5 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 imageRes=R.drawable.ic_android;
             }
         });
+
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.textField.setText(name));
+    }
+
 }
